1071cdcd77eda5e3604ea50252bd8babe244f1e9
PeerChristensen <hr.pchristensen@gmail.com>
2018-12-27 18:14:28 +0100

    heatmap2

--- sou_heatmap.R
+++ sou_heatmap.R
@@ -1,4 +1,6 @@
-
+library(tidytext)
+library(tidyverse)
+library(magrittr)
 
 df <- read_csv("state_of_the_union.csv")
 
@@ -17,21 +19,29 @@ df_dtm_pres <- df               %>%
 
 model <- LDA(df_dtm_pres,k=2, method = "VEM")
 
+#Determine the posterior probabilities of the topics for each document and of the terms for each topic for a fitted topic model.
 d=topicmodels::posterior(model, df_dtm_pres)[["topics"]]
 
-heat_df <- d %>% 
+heat_df <- d   %>% 
   data.frame() %>%
   mutate(president = rownames(d)) %>%
   mutate(president = unique(df$president))
 
-heat_df %<>% gather(topic, value, -president) %>%
+heat_df                                 %<>% 
+  gather(topic, value, -president)      %>%
   mutate(topic = as.factor(as.numeric(str_replace(topic,"X",""))),
-         president = factor(president)) %>% as_tibble()
+         president = factor(president)) %>%
+  as_tibble()
 
 heat_df$president <- factor(heat_df$president, levels = unique(df$president))
 
 heat_df %>% ggplot(aes(topic,fct_rev(president))) + 
-  geom_tile(aes(fill = value), colour = "white") + 
-  scale_fill_gradient(low = "snow", high = "darkred")
+  geom_tile(aes(fill = value), colour = "snow") + 
+  #scale_fill_stellenbosch(discrete = F, "wine") +
+  scale_fill_gradient(low = "snow", high = "darkred") +
+  labs(title = "Topics associated with presidents",
+       y     = "presidents") +
+  theme_minimal()
 
+ggsave("heatmap.png")
   
--- sou_party_classification.R
+++ sou_party_classification.R
@@ -1,4 +1,68 @@
 
+library(tidyverse)
+library(tidytext)
+library(magrittr)
+library(caret)
+library(glue)
 
-#get sou data
+
+df <- read_csv("state_of_the_union.csv")
+
+df                                         %<>%
+  filter(party == "Republican" | party == "Democrat") %>%
+  unnest_tokens(word, text)                %>%
+  anti_join(get_stopwords())               %>%
+  filter(!str_detect(word, "[0-9]+ | NA")) %>%
+  filter(!word %in% c("united","states","must","may","can")) %>%
+  add_count(word)                          %>%
+  filter(n > 10)                           %>%
+  select(-n, -president, -date)            %>%
+  mutate(party = as.factor(ifelse(party == "Republican",1,0)))
+  
+  #group_by(document) %>%
+  #mutate(text = collapse(word,sep=" ")) %>%
+  #ungroup() %>%
+  #select(-word) %>%
+  #distinct()
+
+set.seed(42)
+
+idx <- createDataPartition(df$party, 
+                           p = 0.8, 
+                           list = FALSE, times = 1)
+
+df_train <- df[ idx,]
+df_test  <- df[-idx,]
+
+train_dtm <- df_train       %>%
+  dplyr::count(party, word) %>%
+  cast_dtm(party, word, n)
+
+test_dtm <- df_test         %>%
+  dplyr::count(party, word) %>%
+  cast_dtm(party, word, n)
+
+df_train = as.matrix(df_train)
+
+train <- as.matrix(train_dtm)
+train <- cbind(train, c(0, 1))
+
+colnames(train)[ncol(train)] <- 'y'
+train <- as.data.frame(train)
+train$y <- as.factor(train$y)
+
+
+
+
+xgb_model <- xgb.train(list(max_depth = 7, 
+                            eta = 0.1, 
+                            objective = "binary:logistic",
+                            eval_metric = "error", nthread = 1),
+                            xgb.DMatrix(train_dtm, 
+                                   label = df_train$party),
+                            nrounds = 50)
+
+
+RTextTools
+                           
 
